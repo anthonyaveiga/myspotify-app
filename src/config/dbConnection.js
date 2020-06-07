badfb7063ea9f418a3452fea192f6d5e58208841
anthonyaveiga <45562713+anthonyaveiga@users.noreply.github.com>
2020-06-06 22:27:42 -0500

    Update dbConnection.js

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 module.exports = () => {
   return mysql.createConnection({
-    host: '13.84.162.210',
+    host: '1.1.1.1',
     user: 'root',
-    password: 'pass2020',
+    password: 'password',
     database: 'spotify'
   });
 }
